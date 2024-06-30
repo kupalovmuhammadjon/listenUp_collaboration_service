8e9b77574af90d175a4cbffc0bb251f18e4db6d1
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-06-30 12:17:20 +0500

    removed references

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -3,25 +3,25 @@ CREATE TYPE invitation_status AS ENUM ('pending', 'accepted', 'declined');
 
 CREATE TABLE collaborations (
     id uuid PRIMARY KEY,
-    composition_id uuid REFERENCES compositions(id),
-    user_id uuid REFERENCES users(id),
+    podcast_id uuid,
+    user_id uuid,
     role collaboration_role DEFAULT 'collaborator',
     joined_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE invitations (
     id uuid PRIMARY KEY,
-    composition_id uuid REFERENCES compositions(id),
-    inviter_id uuid REFERENCES users(id),
-    invitee_id uuid REFERENCES users(id),
+    podcast_id uuid,
+    inviter_id uuid,
+    invitee_id uuid,
     status invitation_status DEFAULT 'pending',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE comments (
     id uuid PRIMARY KEY,
-    composition_id uuid REFERENCES compositions(id),
-    user_id uuid REFERENCES users(id),
+    podcast_id uuid,
+    user_id uuid,
     content TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
