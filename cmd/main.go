23127b30bcc1333c490bf9cf8b975d806c371ecb
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-06-30 16:39:42 +0500

    cmd and service methods added

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"collaboration_service/config"
+	pbCollab "collaboration_service/genproto/collaborations"
+	pbCom "collaboration_service/genproto/comments"
+	"collaboration_service/service"
+	"fmt"
+
+	"collaboration_service/storage/postgres"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", config.Load().COLLABORATION_SERVICE_PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := postgres.ConnectDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	server := grpc.NewServer()
+	pbCollab.RegisterCollaborationsServer(server, service.NewCollaborations(db))
+	pbCom.RegisterCommentsServer(server, service.NewComments(db))
+
+	fmt.Printf("server is listening on port %s", config.Load().COLLABORATION_SERVICE_PORT)
+	if err := server.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
+}
--- service/collaborations.go
+++ service/collaborations.go
@@ -3,6 +3,7 @@ package service
 import (
 	pb "collaboration_service/genproto/collaborations"
 	"collaboration_service/storage/postgres"
+	"context"
 	"database/sql"
 )
 
@@ -14,4 +15,22 @@ type Collaborations struct {
 func NewCollaborations(db *sql.DB) *Collaborations {
 	collaborations := postgres.NewCollaborationRepo(db)
 	return &Collaborations{Collaborations: collaborations}
+}
+
+func (c *Collaborations) RespondInvitation(ctx context.Context, req *pb.CreateCollaboration) (*pb.ID, error) {
+	resp, err := c.Collaborations.RespondInvitation(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
+}
+
+func (c *Collaborations) DeleteCollaboratorByPodcastId(ctx context.Context, req *pb.Ids) (*pb.Void, error) {
+	resp, err := c.Collaborations.DeleteCollaboratorByPodcastId(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
