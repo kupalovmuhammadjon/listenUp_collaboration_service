2a8ed52a2dc62c28b4a7f0bf2941e26b6e534572
Umaroff07 <ibrohimumarov0702@gmail.com>
2024-06-30 12:14:27 +0500

    ids changed into uuids

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -2,26 +2,26 @@ CREATE TYPE collaboration_role AS ENUM ('owner', 'collaborator', 'viewer');
 CREATE TYPE invitation_status AS ENUM ('pending', 'accepted', 'declined');
 
 CREATE TABLE collaborations (
-    id SERIAL PRIMARY KEY,
-    composition_id INTEGER REFERENCES compositions(id),
-    user_id INTEGER REFERENCES users(id),
+    id uuid PRIMARY KEY,
+    composition_id uuid REFERENCES compositions(id),
+    user_id uuid REFERENCES users(id),
     role collaboration_role DEFAULT 'collaborator',
     joined_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE invitations (
-    id SERIAL PRIMARY KEY,
-    composition_id INTEGER REFERENCES compositions(id),
-    inviter_id INTEGER REFERENCES users(id),
-    invitee_id INTEGER REFERENCES users(id),
+    id uuid PRIMARY KEY,
+    composition_id uuid REFERENCES compositions(id),
+    inviter_id uuid REFERENCES users(id),
+    invitee_id uuid REFERENCES users(id),
     status invitation_status DEFAULT 'pending',
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE comments (
-    id SERIAL PRIMARY KEY,
-    composition_id INTEGER REFERENCES compositions(id),
-    user_id INTEGER REFERENCES users(id),
+    id uuid PRIMARY KEY,
+    composition_id uuid REFERENCES compositions(id),
+    user_id uuid REFERENCES users(id),
     content TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
